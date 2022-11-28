8afe98f251049a882f53c8bb5b9c2410e97158d5
msrocka <michael.srocka@gmail.com>
2022-11-28 14:14:14 +0100

    test with unit groups

--- backto1/conv.py
+++ backto1/conv.py
@@ -1,12 +1,14 @@
+import json
 import logging as log
 import types
 import uuid
 import zipfile
+
 from dataclasses import dataclass
 from pathlib import Path
 from typing import cast, Any, Optional
 
-import olca_schema as o
+import olca_schema as lca
 from olca_schema import zipio
 
 
@@ -40,38 +42,153 @@ class _Category:
 
 class _Conv:
     def __init__(self, input: Path, output: Path):
-        self._in = zipio.ZipReader(input)
-        self._out = zipfile.ZipFile(
+        self.inp = zipio.ZipReader(input)
+        self.out = zipfile.ZipFile(
             output, mode="a", compression=zipfile.ZIP_DEFLATED
         )
-        self._cats: dict[o.ModelType, dict[str, dict[str, str]]] = {}
+        self.categories: dict[str, dict[str, _Category]] = {}
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type: Any, value: Any, traceback: Any):
+        self.close()
 
     def run(self):
-        pass
+        types = [lca.UnitGroup]
+        for type_ in types:
+            count = 0
+            for e in self.inp.read_each(type_):
+                d = e.to_dict()
+                category = self.category(e)
+                d["category"] = category.to_dict() if category else None
+                self._put(_folder_of(e), d)
+                count += 1
+            log.info("converted %i instances of %s", count, type_.__name__)
 
     def close(self):
-        self._in.close()
-        self._out.close()
+        self.inp.close()
+        self.out.close()
 
-    def base_conv(e: o.RootEntity) -> dict[str, Any]:
-        d = e.to_dict()
-        return d
+    def category(self, e: lca.RootEntity) -> _Category | None:
+        path = e.category
+        if path is None or path.strip() == "":
+            return None
+        model_type = _model_type_of(e)
+        pool = self.categories.get(model_type)
+        if pool is None:
+            pool = {}
+            self.categories[model_type] = pool
+
+        segments = [s.strip() for s in path.split("/")]
+        category: _Category | None = None
+        walked = ""
+        for seg in segments:
+            walked += "/" + seg.lower()
+            next_ = pool.get(walked)
+            if next_ is not None:
+                category = next_
+                continue
+            next_ = _Category(
+                name=seg,
+                model_type=model_type,
+                parent=category,
+            )
+            self._put("categories", next_.to_dict())
+            pool[walked] = next_
+            category = next_
+        return category
+
+    def _put(self, folder: str, entry: dict[str, Any]):
+        uid = entry.get("@id")
+        if uid is None:
+            log.error("@id missing in entry")
+            return
+        s = json.dumps(entry, indent="  ")
+        self.out.writestr(f"{folder}/{uid}.json", s)
 
 
 def convert(input: Path, output: Path):
     log.info("convert %s to %s", input, output)
-
-    with zipfile.ZipFile(
-        output, mode="a", compression=zipfile.ZIP_DEFLATED
-    ) as zf:
-        with zipio.ZipReader(input) as zipin:
-            count = 0
-            for group in zipin.read_each(o.UnitGroup):
-                count += 1
-                _put(group, zf)
-            log.info("copied %i unit groups", count)
+    with _Conv(input, output) as conv:
+        conv.run()
 
 
-def _put(e: o.RootEntity, zf: zipfile.ZipFile):
-    path = zipio._folder_of_entity(e)
-    zf.writestr(f"{path}/{e.id}.json", e.to_json())
+def _model_type_of(e: lca.RootEntity) -> str:
+    match e.__class__:
+        case lca.Actor:
+            return lca.ModelType.ACTOR.value
+        case lca.Currency:
+            return lca.ModelType.CURRENCY.value
+        case lca.DQSystem:
+            return lca.ModelType.DQ_SYSTEM.value
+        case lca.Epd:
+            return lca.ModelType.EPD.value
+        case lca.Flow:
+            return lca.ModelType.FLOW.value
+        case lca.FlowProperty:
+            return lca.ModelType.FLOW_PROPERTY.value
+        case lca.ImpactCategory:
+            return lca.ModelType.IMPACT_CATEGORY.value
+        case lca.ImpactMethod:
+            return lca.ModelType.IMPACT_METHOD.value
+        case lca.Location:
+            return lca.ModelType.LOCATION.value
+        case lca.Parameter:
+            return lca.ModelType.PARAMETER.value
+        case lca.Process:
+            return lca.ModelType.PROCESS.value
+        case lca.ProductSystem:
+            return lca.ModelType.PRODUCT_SYSTEM.value
+        case lca.Project:
+            return lca.ModelType.PROJECT.value
+        case lca.Result:
+            return lca.ModelType.RESULT.value
+        case lca.SocialIndicator:
+            return lca.ModelType.SOCIAL_INDICATOR.value
+        case lca.Source:
+            return lca.ModelType.SOURCE.value
+        case lca.UnitGroup:
+            return lca.ModelType.UNIT_GROUP.value
+        case _:
+            return "UNKNOWN"
+
+
+def _folder_of(e: lca.RootEntity) -> str:
+    match e.__class__:
+        case lca.Actor:
+            return "actors"
+        case lca.Currency:
+            return "currencies"
+        case lca.DQSystem:
+            return "dq_systems"
+        case lca.Epd:
+            return "epds"
+        case lca.Flow:
+            return "flows"
+        case lca.FlowProperty:
+            return "flow_properties"
+        case lca.ImpactCategory:
+            return "lcia_categories"
+        case lca.ImpactMethod:
+            return "lcia_methods"
+        case lca.Location:
+            return "locations"
+        case lca.Parameter:
+            return "parameters"
+        case lca.Process:
+            return "processes"
+        case lca.ProductSystem:
+            return "product_systems"
+        case lca.Project:
+            return "projects"
+        case lca.Result:
+            return "results"
+        case lca.SocialIndicator:
+            return "social_indicators"
+        case lca.Source:
+            return "sources"
+        case lca.UnitGroup:
+            return "unit_groups"
+        case _:
+            return "unknown"
